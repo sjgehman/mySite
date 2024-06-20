0c7dd76f70daff3816fac552087ce54055a32570
sjgehman <samuelgehman@gmail.com>
2024-06-20 01:27:56 -0500

    fixed config bug

---
+++ src/routes/blog/post.svelte
@@ -0,0 +1,14 @@
+<!-- Just like our +layout.svelte file, this component will wrap posts, as a parent component, and slot the post in as its child. The difference is: it’ll work just on Markdown posts in the blog, not every route. The router will ignore post.svelte, even though it’s inside the routes folder, because it’s not a valid + file.
+ -->
+<script>
+	export let title;
+	export let date;
+</script>
+
+<article>
+	<h1>{title}</h1>
+
+	<p>Published: {date}</p>
+
+	<slot />
+</article>
--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,22 @@
 import adapter from '@sveltejs/adapter-auto';
-import SveltePreprocess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
-
-	preprocess: [
-		SveltePreprocess(),
-		mdsvex({
-			extensions: ['.md']
-		})
-	]
+  extensions: ['.svelte', '.md'],
+  kit: {
+    adapter: adapter(),
+  },
+  preprocess: [
+    sveltePreprocess(),
+    mdsvex({
+		extension: '.md',
+		layout: {
+			blog: 'src/routes/blog/post.svelte'
+		}
+    }),
+  ],
 };
 
 export default config;
